51932873cce7c46828ef55561ebbad70d612b061
brunol97 <bruno.lenderink@gmail.com>
2021-11-26 18:50:50 +0100

    comments on regression

--- Poisson regression - students data.r
+++ Poisson regression - students data.r
@@ -49,6 +49,7 @@ prop.table(table(dataset$higher,dataset$sex), 1 )
 chisq.test(table(dataset$higher,dataset$sex))
 
 #correlation between numeric variables
+#Here we plot the correlation matrix of all numeric vaiables using the corrplot package
 num_variables = select_if(X, is.numeric)
 cor(num_variables, Y, method = "spearman")
 
@@ -61,17 +62,20 @@ corrplot(cor_matr, type = "upper", order = "hclust",
 
 
 # Does it look like a Poisson?
+# Here we create a plot to check whether the outcome variable is Poisson distributed
 prob <- dpois(dataset$G3, mean(dataset$G3))
 prob
 plot(dataset$G3, prob, pch = 16, col = "orange", type = "h")
 points(dataset$G3, prob, pch = 16, col = "orange", type = "p")
-
+# From the graph we see it does look like a Poisson distribution
 
 #####POISSON REGRESSION MODEL
-
+# Here we regress the outcome variable G3 on all other variables except G1 and G2 using Poisson regression
 mod = glm(Y ~ ., data=X, family=poisson)
 summary(mod)
 
+#H Here we perform stepwise variable selection to find the best model
+# We finish with an AIC of 3313.1
 best_mod = step(glm(Y ~ ., data=X, family=poisson))
 summary(best_mod)
 
@@ -82,6 +86,7 @@ exp((best_mod$coefficients))
 1-pchisq(best_mod$deviance, best_mod$df.residual) #good fit
 
 #residuals analysis
+#Here we plot the residuals in a scatterplot and then a histogram showing how the residuals are distributed, first usig Pearson, then Deviance residuals
 resid <- resid(best_mod, type = "pearson")
 fitted <- fitted(best_mod)
 plot(fitted, resid, main = "Pearson residuals")
@@ -96,10 +101,11 @@ hist(resid)
 plot(best_mod, which = 1) # Pearson residuals
 
 ##### prediction
+#Here we set a random seed for a random train/test split. Then we split up our dataset in 80 % training data and 20 % testing data.
 set.seed(23)
 tr = sample(nrow(X), nrow(X)*0.8)
 
-
+#Here we train our model only using the training data
 train_model <- glm(Y[tr] ~ school + sex + Fedu + studytime + failures + 
                      schoolsup + higher + Dalc + health, data = X[tr,])
 
